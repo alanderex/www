e0c03693369ea7e0e7c4b1ca5caf7f4849ed27b0
Peter Hoffmann <ph@peter-hoffmann.com>
2017-09-02 16:25:56 +0200

    use lektor to generate the page

---
+++ README.md
@@ -0,0 +1,21 @@
+PyCon.DE & PyData Karlsruhe 2017
+================================
+
+
+    virtualenv venv
+    source venv/bin/activate
+    pip install -r requirements.txt
+
+
+Initial Setup:
+
+    lektor quickstart
+
+
+Edit Pages
+
+    cd pyconde
+    lektor server
+
+Build Static Site
+    lektor build --output-path ../www2
---
+++ papercall.py
@@ -0,0 +1,118 @@
+import sys
+import json
+import mistune
+from jinja2 import Template
+import re
+from unicodedata import normalize
+import os
+
+
+_punct_re = re.compile(r'[\t !"#$%&\'()*\-/<=>?@\[\\\]^_`{|},.]+')
+_regex = re.compile('[^a-z0-9]')
+#First parameter is the replacement, second parameter is your input string
+
+
+def slugify(text, delim=u'-'):
+    """Generates an slightly worse ASCII-only slug."""
+    result = []
+    for word in _punct_re.split(text.lower()):
+        word = normalize('NFKD', word).encode('ascii', 'ignore')
+        word = word.decode('ascii')
+        word = _regex.sub('', word)
+        if word:
+            result.append(word)
+    return str(delim.join(result))
+
+def enrich(entry):
+    entry["slug"] = slugify(entry["title"])
+    entry["tags_str"] = " ".join(entry["tags"]) 
+    return entry
+
+data = json.load(open("submissions.json"))
+data = list(filter(lambda entry: entry["state"] == "accepted", data))
+data = list(map(enrich, data))
+data.sort(key=lambda entry: entry["title"])
+
+
+tpl = """_model: page_markdown
+---
+title: {{title}}
+---
+body:
+
+# {{title}}
+**{{name}}**
+
+{{bio}}
+
+## Abstract
+{%if tags_str %}
+*Tags:* {{tags_str}}{%endif%}
+
+{{abstract}}
+
+
+## Description
+{{description}}
+
+"""
+
+
+tpl_index = """_model: page_markdown
+---
+title: {{kind|capitalize}}
+---
+body:
+
+{% for entry in data %}
+# [{{entry.title}}](./{{entry.slug}}/)
+**{{entry.name}}**
+
+{{entry.abstract}}
+
+{% endfor %}
+"""
+
+template = Template(tpl)
+template_index = Template(tpl_index)
+
+tutorials = ['practical-data-cleaning-101', 
+             'machine-learning-as-a-service',
+            'metaclasses-when-to-use-and-when-not-to-use',
+            'network-analysis-using-python',
+            'topic-modelling-and-a-lot-more-with-nlp-framework-gensim',
+            'how-to-found-a-company'
+            ]
+
+
+def dump(entry, kind='tutorials'):
+    dirname = 'pyconde/content/schedule/{}/{}/'.format(kind, entry['slug'])
+    print(dirname)
+    if not os.path.isdir(dirname):
+        os.mkdir(dirname)
+    with open(os.path.join(dirname, 'contents.lr'), 'w') as f:
+        f.write(template.render(entry))
+
+
+def gen():
+    d = filter(lambda entry: entry["slug"] in tutorials, data)
+    with open('pyconde/content/schedule/tutorials/contents.lr', 'w') as f:
+        f.write(template_index.render(kind="tutorials", data=d))
+    
+    d = filter(lambda entry: entry["slug"] not in tutorials, data)
+    with open('pyconde/content/schedule/talks/contents.lr', 'w') as f:
+        f.write(template_index.render(kind="talks", data=d))
+
+    for entry in filter(lambda entry: entry["slug"] in tutorials, data):
+        dump(entry)
+        #print(template.render(entry))
+    for entry in filter(lambda entry: entry["slug"] not in tutorials, data):
+        dump(entry, kind='talks')
+
+def main(args=None):
+    if args is None:
+        args = sys.argv
+    gen() 
+
+if __name__ == "__main__":
+    main()
