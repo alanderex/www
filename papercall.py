876407aa04a9a6a5abad7ee89d2a34e0f449cc81
Peter Hoffmann <ph@peter-hoffmann.com>
2017-10-22 13:12:19 +0200

    update paperall.py script

--- papercall.py
+++ papercall.py
@@ -123,7 +123,7 @@ tutorials = ['practical-data-cleaning-101',
              'network-analysis-using-python',
              'topic-modelling-and-a-lot-more-with-nlp-framework-gensim',
              'python-on-bare-metal-beginners-tutorial-with-micropython-on-the-pyboard',
-             'how-to-fund-your-company'
+             'how-to-fund-your-company',
              ]
 
 
